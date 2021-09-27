99d78528a37b52d3409a333903c74d2934a7eff3
Leo Miranda <leonardo_miranda@icloud.com>
2021-09-26 22:40:59 -0300

    adicionando arquivos

--- iMarket/src/main/java/br/com/market/imarket/IMarketApplication.java
+++ iMarket/src/main/java/br/com/market/imarket/IMarketApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IMarketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IMarketApplication.class, args);
-		//comment
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IMarketApplication.class, args);
+    }
 
 }
