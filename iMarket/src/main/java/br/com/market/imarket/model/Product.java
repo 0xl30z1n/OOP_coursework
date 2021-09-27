9e71c5990fcf0f0bba266f0cecc74213b2f3f2fc
Leo Miranda <leonardo_miranda@icloud.com>
2021-09-26 23:47:36 -0300

    alterando arquivos
    arquivos referentes as classes do banco de dados

--- iMarket/src/main/java/br/com/market/imarket/model/Product.java
+++ iMarket/src/main/java/br/com/market/imarket/model/Product.java
@@ -1,4 +1,62 @@
 package br.com.market.imarket.model;
 
+import javax.persistence.*;
+
+@Table
 public class Product {
+
+    private long id;
+    private String name;
+    private String brand;
+    private float price;
+    private int quantity;
+    private String description;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
---
+++ iMarket/src/main/java/br/com/market/imarket/repository/OrderRepository.java
@@ -0,0 +1,4 @@
+package br.com.market.imarket.repository;
+
+public interface OrderRepository {
+}
