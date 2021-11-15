8fdb0bb501c90cabb06a20c0df42c604eb1e6275
rodrigo-222 <66509029+rodrigo-222@users.noreply.github.com>
2021-11-14 22:55:02 -0300

    update-

--- imarket/src/main/java/br/iesb/imarket/model/builder/ProductBuilder.java
+++ imarket/src/main/java/br/iesb/imarket/model/builder/ProductBuilder.java
@@ -1,5 +1,58 @@
 package br.iesb.imarket.model.builder;
 
 public class ProductBuilder {
+    private final Product product;
+
+    private ProductBuilder(){
+        this.product = new Product();
+    }
+
+    public static ProductBuilder builder(){
+        return new ProductBuilder();
+    }
+
+    public ProductBuilder nameInProduct(String name){
+        product.setName(name);
+        return this;
+    }SS
+
+    public ProductBuilder brandInProduct(String brand){
+        product.setbrand(brand);
+        return this;
+    }
+
+    public ProductBuilder withPrice(float price){
+        product.setprice(price);
+        return this;
+    }
+
+    public ProductBuilder descriptionInProduct(String description){
+        product.setdescription(description);
+        return this;
+    }
+
+    public ProductBuilder withPromotion(boolean promotion){
+        product.setpromotion(price);
+        return this;
+    }
+
+    public ProductBuilder withPercent(float percent){
+        product.setpercent(percent);
+        return this;
+    }
+
+    public ProductBuilder withCategory(CategoryType category){
+        product.setCategory(category);
+        return this;
+    }
+
+    public ProductBuilder withQuantity(int quantity){
+        product.setquantity(quantity);
+        return this;
+    }
+
+    public Product build(){
+        return this.product;
+    }
 
 }
